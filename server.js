61bbc45c49d531dbd9923f30fd60b7924ef9b113
VISHNU M K <futuralabs.mern@gmail.com>
2024-11-07 11:12:11 +0530

    http post method

--- server.js
+++ server.js
@@ -1,5 +1,5 @@
 const http = require("http")
-const url = require("url") // build-in module
+const url = require("url")
 
 const server = http.createServer()
 
@@ -7,18 +7,22 @@ server.on("request", async (request, response) => {
 
     const path = request.url
     
-    const { pathname, query } = url.parse(path, true)
+    const { pathname } = url.parse(path, true)
 
     if (pathname == "/api") {
-        const { id } = query
-        if (id < 1) {
-            return response.end("not found")
-        }
-        const res = await fetch(`https://jsonplaceholder.typicode.com/todos/${id}`)
-        const result = await res.json()
-        response.writeHead(200, { "Content-Type": "application/json" })
-        response.write(JSON.stringify(result))
-        response.end()
+        let obj;
+        request.on("data", (body) => {
+            obj = Buffer.from(body).toString()
+        })
+        request.on("end", async () => {
+            const data = JSON.parse(obj)
+            const res = await fetch(`https://jsonplaceholder.typicode.com/todos/${data.id}`)
+            const result = await res.json()
+            response.writeHead(200, { "Content-Type": "application/json" })
+            response.write(JSON.stringify(result))
+            response.end()
+        })
+        
     }
 
 })
